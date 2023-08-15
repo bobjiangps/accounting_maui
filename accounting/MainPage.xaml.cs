71132b7bf6490249696466d48c0cd0c1f0f21a5e
Bo Jiang <bo.jiang@activenetwork.com>
2023-08-15 13:52:49 +0800

    change after check system platform

--- accounting/MainPage.xaml.cs
+++ accounting/MainPage.xaml.cs
@@ -16,6 +16,11 @@ public partial class MainPage : ContentPage
         {
             editor.Text = "Enter your note; no file in:" + _fileName;
         }
+        systemHint.Text += DeviceInfo.Platform.ToString();
+        MyStackLayout.Margin =
+            DeviceInfo.Platform == DevicePlatform.iOS
+                ? new Thickness(30, 60, 30, 30) // Shift down by 60 points on iOS only
+                : new Thickness(30); // Set the default margin to be 30 points
     }
 
 	private void SaveClicked(object sender, EventArgs e)
