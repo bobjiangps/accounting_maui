0dc34cfe3bf64d64bf90d8562bb6421bf9a45372
Bo Jiang <bo.jiang@activenetwork.com>
2023-08-29 18:22:47 +0800

    use fontawesome icon in project

--- accounting/MauiProgram.cs
+++ accounting/MauiProgram.cs
@@ -13,7 +13,10 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                //fonts.AddFont("Font Awesome 6 Brands-Regular-400.otf", "FA-B");
+                //fonts.AddFont("Font Awesome 6 Free-Regular-400.otf", "FA-R");
+                fonts.AddFont("Font Awesome 6 Free-Solid-900.otf", "FA-S");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
